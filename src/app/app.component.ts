f176c8645c28ec20f092846a3f6701784f0e7827
Adjectival <alexd.jacks@gmail.com>
2018-03-01 18:56:24 -0800

    :construction: adds main component w values, needs binders and emitters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,29 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  template: `
+  <h1 class="mt-1">FontAwesome Masker</h1>
+  
+    <div class="card p-3 col-8 offset-2">
+    <div class="fa-3x">
+      <i class="fas fa-star" data-fa-transform="shrink-10 up-0.1" data-fa-mask="fas fa-cloud"></i>
+    </div>
+    
+    <input type="text" [value]="main"><label for="main">Main Icon</label>
+    <input type="text" [value]="mask"><label for="mask">Mask Icon</label>
+    <input type="text" [value]="sizer"><label for="sizer">Size Change</label>
+    <input type="text" [value]="upDown"><label for="up">Up/Down Change</label>
+
+    <button type="button" name="button" class="btn mt-3">
+        <h2>Save? When Express & Mongo arrive...</h2>
+    </button>
+    </div>
+  `
 })
 export class AppComponent {
-  title = 'app';
+  main: string = 'star';
+  mask: string = 'cloud';
+  sizer: number = 10;
+  upDown: number = 0.1;
 }
